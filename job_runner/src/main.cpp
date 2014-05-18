48f9090a69d6b810739b9bd9ae23f55a2df549f6
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-05-17 21:08:35 -0700

    Simple stuff.
    - Added skeleton ZMQ stuff
    - Fail if the URI is wrong

--- job_runner/src/main.cpp
+++ job_runner/src/main.cpp
@@ -1,5 +1,10 @@
+#include <iostream>
 #include "waifu.h"
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "You need to specify a URI.\n";
+        return -1;
+    }
     return waifu::main_loop(argc, argv);
 }
--- job_runner/src/waifu.cpp
+++ job_runner/src/waifu.cpp
@@ -1,5 +1,27 @@
+#include <iostream>
+#include <zmq.hpp>
 #include "waifu.h"
 
 int waifu::main_loop(int argc, char *argv[]) {
+    // Prepare our context and socket
+    zmq::context_t context(1);
+    zmq::socket_t socket(context, ZMQ_REP);
+    socket.bind("tcp://*:5555");
+
+    while (true) {
+        zmq::message_t request;
+
+        // Wait for next request from client
+        socket.recv(&request);
+        std::cout << "Received Hello" << std::endl;
+
+        //sleep(1);
+
+        // Send reply back to client
+        zmq::message_t reply (5);
+        memcpy((void *)reply.data(), "World", 5);
+        socket.send (reply);
+    }
+
     return 0;
 }
