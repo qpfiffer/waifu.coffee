8912945e9fc3d46bcbfc66609cd47ae297c98be9
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-05-24 23:26:38 -0700

    Fuck it we're using imagemagick.

--- job_runner/src/worker.cpp
+++ job_runner/src/worker.cpp
@@ -17,52 +17,6 @@ worker::~worker() {
 }
 
 bool worker::compute_jpeg_coeef(const string &filepath) {
-    const int COLOR_COMPONENTS = 3;
-    // Get the file size so we know how big to build our in-memory buffer
-    const int file_size = utils::get_file_size(filepath);
-    cout << "[-] Worker opening file of size " << file_size << endl;
-
-    // Open jpeg
-    int fd;
-    fd = open(filepath.c_str(), O_RDONLY);
-    assert(fd != -1);
-
-    // Map the file into memory
-    void *addr = NULL;
-    addr = mmap(NULL, (size_t)file_size, PROT_READ, MAP_PRIVATE, fd, 0);
-
-    int jpeg_subsamp, width, height;
-    // Init turbo jpeg decompressor
-    tjhandle tj_decompress = tjInitDecompress();
-
-    tjDecompressHeader2(tj_decompress, (unsigned char *)addr, file_size,
-                        &width, &height, &jpeg_subsamp);
-
-    cout << "[-] Worker has opened file of width " << width
-         << " and height " << height << endl;
-    // Now that we know the width and height of our image we can allocate
-    // our decompressed buffer
-    const int DECOMPRESSED_SIZE = width * height * COLOR_COMPONENTS;
-    unsigned char *memory_buffer = new unsigned char[DECOMPRESSED_SIZE];
-    memset(memory_buffer, '\0', DECOMPRESSED_SIZE);
-
-    // Decompress into memory_buffer
-    tjDecompress2(tj_decompress, (unsigned char *)addr, file_size, memory_buffer,
-                  width, 0, height, TJPF_RGB, TJFLAG_FASTDCT);
-
-    // So now that we have an array of RGB components in memory, we have to do
-    // stuff with it. We first need to scale the image down to what Haar2D
-    // expects:
-    unsigned char *scaled_image = new unsigned char[NUM_PIXELS_SQUARED * 3];
-    utils::scale_image(memory_buffer, width, height, scaled_image);
-
-    // Tidy up
-    tjDestroy(tj_decompress);
-    delete[] scaled_image;
-    delete[] memory_buffer;
-    munmap(addr, (size_t)file_size);
-    close(fd);
-
     return true;
 }
 
