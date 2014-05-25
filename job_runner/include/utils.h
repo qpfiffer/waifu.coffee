1bf7e6eafe068c2c93aca8fe81b6c46828954e0a
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-05-24 17:49:55 -0700

    Niave image scaling.

--- job_runner/include/utils.h
+++ job_runner/include/utils.h
@@ -5,6 +5,9 @@ namespace waifu {
     namespace utils {
         // Gets the size of a file in bytes. Probably.
         int get_file_size(const std::string &filepath);
+        // Scales an image to what Haar2D expects. Fills out dest_array.
+        void scale_image(const unsigned char *source_array, const int &s_width, const int &s_height,
+                         unsigned char *dest_array);
         // Turns message_t objects into msgpack unpacked/object objects.
         void zmq_to_msgpack(zmq::message_t *request, msgpack::unpacked *unpacked_body, msgpack::object *obj);
     }
--- job_runner/src/worker.cpp
+++ job_runner/src/worker.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <turbojpeg.h>
 #include <unistd.h>
+#include "haar.h"
 #include "utils.h"
 #include "waifu.h"
 
@@ -15,7 +16,7 @@ worker::worker(Job job) : main_job(job), main_thread(std::thread(&worker::work, 
 worker::~worker() {
 }
 
-bool worker::decompose_jpeg(const string &filepath) {
+bool worker::compute_jpeg_coeef(const string &filepath) {
     const int COLOR_COMPONENTS = 3;
     // Get the file size so we know how big to build our in-memory buffer
     const int file_size = utils::get_file_size(filepath);
@@ -48,8 +49,16 @@ bool worker::decompose_jpeg(const string &filepath) {
     // Decompress into memory_buffer
     tjDecompress2(tj_decompress, (unsigned char *)addr, file_size, memory_buffer,
                   width, 0, height, TJPF_RGB, TJFLAG_FASTDCT);
+
+    // So now that we have an array of RGB components in memory, we have to do
+    // stuff with it. We first need to scale the image down to what Haar2D
+    // expects:
+    unsigned char *scaled_image = new unsigned char[NUM_PIXELS_SQUARED * 3];
+    utils::scale_image(memory_buffer, width, height, scaled_image);
+
     // Tidy up
     tjDestroy(tj_decompress);
+    delete[] scaled_image;
     delete[] memory_buffer;
     munmap(addr, (size_t)file_size);
     close(fd);
@@ -66,8 +75,8 @@ void worker::work() {
         // 1. Load image from filesystem
         // 2. Get coefficients
         string filename = this->main_job["filepath"];
-        this->decompose_jpeg(filename);
-        // 3. Loop through all images in the DB and find similar ones
+        this->compute_jpeg_coeef(filename);
+        // 3. Parallel map/reduce through all images in the DB and find similar ones 
         // 4. Store new image in scored images DB
         // 5. Send results off to main loop
         // 6. Fucking kill self
